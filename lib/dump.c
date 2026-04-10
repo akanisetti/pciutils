e153dbda9a5df6897863750b8a57f2c0c693f4fd
Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>
2026-04-10 17:39:36 +0000

    [COVERITY] pciutils-intel: fix Copy of overlapping memory (1 finding)
    CID 6163048: dump_init uses memcpy to copy config space data after
    re-allocating the buffer; if dump_alloc_data returns the same address
    the source and destination overlap, causing undefined behavior.
    
    Replaced memcpy with memmove which is safe for overlapping regions.
    
    Tracked-On: NIACP3-1264
    Signed-off-by: Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>

--- lib/dump.c
+++ lib/dump.c
@@ -106,7 +106,7 @@ dump_init(struct pci_access *a)
 	      if (i >= dd->allocated)	/* Need to re-allocate the buffer */
 		{
 		  dump_alloc_data(dev, 4096);
-		  memcpy(((struct dump_data *) dev->aux)->data, dd->data, 256);
+      memmove(((struct dump_data *) dev->aux)->data, dd->data, 256);
 		  pci_mfree(dd);
 		  dd = dev->aux;
 		}
